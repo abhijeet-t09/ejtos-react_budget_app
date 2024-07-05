4e8ca188816be6c4903262b395bf57e35cca7929
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2024-07-05 13:35:43 -0400

    Editable Budget Update

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,16 +2,39 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, expenses, dispatch } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        const newBudgetValue = parseInt(event.target.value, 10);
+        const totalExpenses = expenses.reduce((total, item) => {
+            return (total += item.cost);
+        }, 0);
+
+        if (newBudgetValue > 20000) {
+            alert("The budget cannot exceed remaining funds £20,000");
+        } else if (newBudgetValue < totalExpenses) {
+            alert("You cannot reduce the budget value lower than the spending");
+        } else {
+            setNewBudget(newBudgetValue);
+            dispatch({
+                type: 'SET_BUDGET',
+                payload: newBudgetValue
+            });
+        }
     }
+
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+        <div className='alert alert-secondary'>
+            <span>Budget: £{budget}</span>
+            <input
+                type="number"
+                step="10"
+                value={newBudget}
+                onChange={handleBudgetChange}
+            />
+        </div>
     );
 };
+
 export default Budget;
